814d67e8c6d6cc9f75190a5982c4189bccac94fa
Malvado996 <malvado996@gmail.com>
2021-04-01 18:34:34 -0400

    added foodpantry model schema

---
+++ models/Foodpantry.js
@@ -0,0 +1,66 @@
+const mongoose = require(`mongoose`);
+
+const FoodpantrySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name'],
+        unique: true, 
+        trim: true,
+        maxlength: [50, 'Name can not be more than 50 characters']
+    },
+    slug: String,
+    description: {
+        type: String,
+        required: [true, 'Please add a description'],
+        maxlength: [500, 'Description can not be more than 500 characters']
+    },
+    website: {
+        type: String,
+        match: [
+            /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/,
+            'Please use a valid URL with HTTP or HTTPS'
+        ],
+    },
+    phone: {
+        type: String,
+        maxlength: [20, 'Phone number can not be longer than 20 characters']
+    },
+    email: {
+        type: String,
+        match: [
+            /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+            'Please add a valid email' 
+        ]
+    },
+    address: {
+        type: String,
+        required: [true, 'Please add an address']
+    },
+    location: {
+        // GeoJSON Point
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: false
+          },
+          coordinates: {
+            type: [Number],
+            required: false,
+            index: '2dsphere'
+          },
+          formattedAddress: String,
+          street: String,
+          city: String,
+          state: String,
+          zipcode: String,
+          country: String,
+    },
+    photo: {
+        type: String,
+        default: 'no-photo.jpg'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+}); 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const color = require(`colors`);
 const connectDB = require('./config/db');
 
 // Load .env vars
@@ -26,12 +27,12 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
     PORT, 
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.brightBlue.bold)
 );
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-    console.log(`ERROR: ${err.message}`);
+    console.log(`ERROR: ${err.message}`.brightYellow.bold);
     // Close server & exit process
     server.close(() => process.exit(1));
 })
