a236c978d8764c108c036cf0f550e47e5800c270
Malvado996 <malvado996@gmail.com>
2021-04-03 15:14:46 -0400

    added geocoder

--- models/Foodpantry.js
+++ models/Foodpantry.js
@@ -1,5 +1,6 @@
 const mongoose = require(`mongoose`);
 const slugify = require(`slugify`);
+const geocoder = require(`../utils/geocoder`);
 
 const FoodpantrySchema = new mongoose.Schema({
     name: {
@@ -72,5 +73,24 @@ FoodpantrySchema.pre('save', function(next) {
     this.slug = slugify(this.name, { lower: true });
     next();
 })
+
+// Geocode and create location field
+FoodpantrySchema.pre(`save`, async function(next) {
+    const loc = await geocoder.geocode(this.address);
+    this.location = {
+        type: `Point`,
+        coordinates: [loc[0].longitude, loc[0].latitude],
+        formattedAddress: loc[0].formattedAddress,
+        street: loc[0].streetName,
+        city: loc[0].city,
+        state: loc[0].stateCode,
+        zipcode: loc[0].zipcode,
+        country: loc[0].countryCode,
+    }
+
+    // Do not save address in DB
+    this.address = undefined;
+    next();
+});
 
 module.exports = mongoose.model('Foodpantry', FoodpantrySchema);
