3d9dbd38c1e108990b6c3f45692202f280bfb2cf
Malvado996 <malvado996@gmail.com>
2021-04-03 14:18:18 -0400

    installed/implemented slugify

--- models/Foodpantry.js
+++ models/Foodpantry.js
@@ -1,4 +1,5 @@
 const mongoose = require(`mongoose`);
+const slugify = require(`slugify`);
 
 const FoodpantrySchema = new mongoose.Schema({
     name: {
@@ -65,5 +66,11 @@ const FoodpantrySchema = new mongoose.Schema({
     },
 
 }); 
+
+// Create foodpantry slug from name
+FoodpantrySchema.pre('save', function(next) {
+    this.slug = slugify(this.name, { lower: true });
+    next();
+})
 
 module.exports = mongoose.model('Foodpantry', FoodpantrySchema);
