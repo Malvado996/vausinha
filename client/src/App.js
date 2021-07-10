56d8467ccd5e0018033183c4cdfea871dd6eddc7
Malvado996 <malvado996@gmail.com>
2021-07-09 21:42:45 -0400

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -4,31 +4,27 @@ import './App.css';
 
 class App extends React.Component {
   
-  componentDidMount() {
-    fetch("/api/v1/foodpantries")
-    .then(response => response.json())
-    .then(data => console.log(data.data[0]));
-  }
 
+    state = {
+      title: 'Shelter'
+    }
+
+    handleFetch = () => {
+        fetch("/api/v1/foodpantries")
+        .then(response => response.json())
+        // .then(data => console.log(data.data[0].name))
+        .then(data => this.setState({title: data.data[0].name}));
+    }
   
     render() {
       return (
       <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div> )
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <button onClick={ this.handleFetch }>Fetch API</button>
+          <p>{ this.state.title }</p>
+        </header>
+      </div> )
     }
   
 }
