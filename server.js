40db1526a81e8aa0dd0766e235d09681f68e4034
Malvado996 <malvado996@gmail.com>
2021-08-25 22:03:25 -0400

    commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const errorHandler = require(`./middleware/error`);
 const connectDB = require('./config/db');
 
 // Load .env vars
-dotenv.config({ path: './config/config.env' }); 
+dotenv.config({ path: './config/config.env' });
 
 // Connect to MongoDB
 connectDB();
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json());
 
 // Dev logging middleware
-if(process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
@@ -33,7 +33,7 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
-    PORT, 
+    PORT,
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.brightBlue.bold)
 );
 
@@ -43,45 +43,6 @@ process.on('unhandledRejection', (err, promise) => {
     // Close server & exit process
     server.close(() => process.exit(1));
 })
-
-// const express = require('express');
-// const dotenv = require('dotenv');
-// const morgan = require('morgan');
-// const color = require(`colors`);
-// const errorHandler = require(`./middleware/error`);
-// const connectDB = require('./config/db');
-
-// // Load .env vars
-// dotenv.config({ path: './config/config.env' }); 
-
-// // Connect to MongoDB
-// connectDB();
-
-// // Route files
-// const foodpantries = require('./routes/foodpantries');
-
-// const app = express();
-
-// // Body parser
-// app.use(express.json());
-
-// // Dev logging middleware
-// if(process.env.NODE_ENV === 'development') {
-//     app.use(morgan('dev'));
-// }
-
-// // Mount routers
-// app.use('/api/v1/foodpantries', foodpantries);
-
-// // Error handling
-// app.use(errorHandler);
-
-// const PORT = process.env.PORT || 5000;
-
-// const server = app.listen(
-//     PORT, 
-//     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.brightBlue.bold)
-// );
 
 // // Handle unhandled promise rejections
 // process.on('unhandledRejection', (err, promise) => {
