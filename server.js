6d7cae9601c59f224b0193be9e92c81617ae146b
Malvado996 <malvado996@gmail.com>
2021-08-30 21:51:28 -0400

    commit

--- server.js
+++ server.js
@@ -44,3 +44,41 @@ process.on('unhandledRejection', (err, promise) => {
     server.close(() => process.exit(1));
 })
 
+// // Load .env vars
+// dotenv.config({ path: './config/config.env' });
+
+// // Connect to MongoDB
+// connectDB();
+
+// // Route files
+// const foodpantries = require('./routes/foodpantries');
+
+// const app = express();
+
+// // Body parser
+// app.use(express.json());
+
+// // Dev logging middleware
+// if (process.env.NODE_ENV === 'development') {
+//     app.use(morgan('dev'));
+// }
+
+// // Mount routers
+// app.use('/api/v1/foodpantries', foodpantries);
+
+// // Error handling
+// app.use(errorHandler);
+
+// const PORT = process.env.PORT || 5000;
+
+// const server = app.listen(
+//     PORT,
+//     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.brightBlue.bold)
+// );
+
+// // Handle unhandled promise rejections
+// process.on('unhandledRejection', (err, promise) => {
+//     console.log(`ERROR: ${err.message}`.brightYellow.bold);
+//     // Close server & exit process
+//     server.close(() => process.exit(1));
+// })
