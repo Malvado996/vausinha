5502c7940135a5fb511992cdff8a028e4b8046bb
Malvado996 <malvado996@gmail.com>
2021-09-13 21:36:26 -0400

    commit

--- server.js
+++ server.js
@@ -44,48 +44,49 @@ process.on('unhandledRejection', (err, promise) => {
     server.close(() => process.exit(1));
 })
 
-// const express = require('express');
-// const dotenv = require('dotenv');
-// const morgan = require('morgan');
-// const color = require(`colors`);
-// const errorHandler = require(`./middleware/error`);
-// const connectDB = require('./config/db');
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const color = require(`colors`);
+const errorHandler = require(`./middleware/error`);
+const connectDB = require('./config/db');
 
-// // Load .env vars
-// dotenv.config({ path: './config/config.env' });
+// Load .env vars
+dotenv.config({ path: './config/config.env' });
 
-// // Connect to MongoDB
-// connectDB();
+// Connect to MongoDB
+connectDB();
 
-// // Route files
-// const foodpantries = require('./routes/foodpantries');
+// Route files
+const foodpantries = require('./routes/foodpantries');
 
-// const app = express();
+const app = express();
 
-// // Body parser
-// app.use(express.json());
+// Body parser
+app.use(express.json());
 
-// // Dev logging middleware
-// if (process.env.NODE_ENV === 'development') {
-//     app.use(morgan('dev'));
-// }
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
-// // Mount routers
-// app.use('/api/v1/foodpantries', foodpantries);
+// Mount routers
+app.use('/api/v1/foodpantries', foodpantries);
 
-// // Error handling
-// app.use(errorHandler);
+// Error handling
+app.use(errorHandler);
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-// const server = app.listen(
-//     PORT,
-//     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.brightBlue.bold)
-// );
+const server = app.listen(
+    PORT,
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.brightBlue.bold)
+);
 
-// // Handle unhandled promise rejections
-// process.on('unhandledRejection', (err, promise) => {
-//     console.log(`ERROR: ${err.message}`.brightYellow.bold);
-//     // Close server & exit process
-//     server.close(() => process.exit(1));
-// })
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`ERROR: ${err.message}`.brightYellow.bold);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+})
+
