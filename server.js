ed906f96a13b05e46f90d924fa19c90822553a4f
Malvado996 <malvado996@gmail.com>
2021-09-21 21:37:04 -0400

    commit

--- server.js
+++ server.js
@@ -44,3 +44,9 @@ process.on('unhandledRejection', (err, promise) => {
     server.close(() => process.exit(1));
 })
 
+// // Handle unhandled promise rejections
+// process.on('unhandledRejection', (err, promise) => {
+//     console.log(`ERROR: ${err.message}`.brightYellow.bold);
+//     // Close server & exit process
+//     server.close(() => process.exit(1));
+// })
