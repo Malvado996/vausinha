b40ce1c8f2569e54a02d4d0fad0e91d6dbc6182c
Malvado996 <malvado996@gmail.com>
2021-09-22 22:06:08 -0400

    commit

--- server.js
+++ server.js
@@ -44,6 +44,22 @@ process.on('unhandledRejection', (err, promise) => {
     server.close(() => process.exit(1));
 })
 
+
+
+
+// // Mount routers
+// app.use('/api/v1/foodpantries', foodpantries);
+
+// // Error handling
+// app.use(errorHandler);
+
+// const PORT = process.env.PORT || 5000;
+
+// const server = app.listen(
+//     PORT,
+//     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.brightBlue.bold)
+// );
+
 // // Handle unhandled promise rejections
 // process.on('unhandledRejection', (err, promise) => {
 //     console.log(`ERROR: ${err.message}`.brightYellow.bold);
